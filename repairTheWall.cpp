e44707f5a1144b037dd900b69355c7d337f20eae
bronyale <bronyale@outlook.com>
2020-07-05 11:23:01 +0800

    俺要重新做人了，先从一个简单的贪心做起 ^_^

--- repairTheWall.cpp
+++ repairTheWall.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
-using namesapce std;
+using namespace std;
+
+int L, N;
+int a[1000];
 
 int main(){
-	cout << "hello world" << endl;
+	while(cin >> L >> N && L * N != 0){
+		int i;
+		int len = 0;
+		for(i = 0; i < N; i ++){
+			cin >> a[i];
+			++ len;
+		}
+		sort(a, a + len, greater<int>());
+		int sum = 0;
+		int cnt = 0;
+		int flag = 0;
+		for(i = 0; i < len; i ++){
+			sum += a[i];
+			++ cnt;
+			if(sum >= L){
+				cout << cnt << endl;
+				flag = 1;
+				break;
+			}
+		}
+		if(!flag){
+			cout << "impossible" << endl;
+		}
+	}
 	
 	return 0;
 }
